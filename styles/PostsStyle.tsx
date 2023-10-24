8f3b2d0237cbf732d52a914cc95adffd6a64421e
FlemmingKrogOlsen <flemming@krogolsen.dk>
2023-10-24 15:59:27 +0100

    Header smaller on mobile

--- styles/PostsStyle.tsx
+++ styles/PostsStyle.tsx
@@ -5,16 +5,19 @@ import styled from 'styled-components'
 export const Section = styled.section`
   margin: 1.5rem auto 0 auto;
   width: 100%;    
-  @media (max-width: 768px) {
+  @media (min-width: 768px) {
     max-width: 1000px;    
   }
 `
 
 export const Header = styled.h2`
-  font-size: 2rem;
+  font-size: 1.5rem;
   font-weight: 700;
   color: white;
-  text-align: center;    
+  text-align: center;
+  @media (min-width: 768px) {
+    font-size: 2rem;
+  }    
 `
 
 export const List = styled.div`
