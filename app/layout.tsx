afa70fc4bc1599367988f43ffe53c6473a2c1d6c
FlemmingKrogOlsen <flemming@krogolsen.dk>
2023-10-25 16:27:46 +0100

    more fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,13 +32,13 @@ export default function RootLayout({ children }: Props) {
         <meta property="og:url" content="https://next-blog-nu-lac.vercel.app/" />
         <meta property="og:title" content="United Real Estate Tanger" />
         <meta property="og:description" content="Building affordable housing in Morocco Tanger" />
-        <meta property="og:image" content={`/images/${imageName}`} />
+        <meta property="og:image" content={`https://next-blog-nu-lac.vercel.app/images/${imageName}`} />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://next-blog-nu-lac.vercel.app/" />
         <meta property="twitter:title" content="United Real Estate Tanger" />
         <meta property="twitter:description" content="Building affordable housing in Morocco Tanger" />
-        <meta property="twitter:image" content={`/images/${imageName}`} />
+        <meta property="twitter:image" content={`https://next-blog-nu-lac.vercel.app/images/${imageName}`} />
 
       </Head>
 
