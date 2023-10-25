c807efec9df63bc4ce5c4861c3d045c462320c4d
FlemmingKrogOlsen <flemming@krogolsen.dk>
2023-10-25 18:43:15 +0100

    meta cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,28 +5,44 @@ import GlobalStyles from '@/styles/GlobalStyle'
 import { Main } from '@/styles/LayoutStyle'
 import HeroBanner from '@/components/HeroBanner'
 import Footer from '@/components/Footer'
-import Head from 'next/head'
 
 type Props = {
   children: React.ReactNode
 }
 
-// export const metadata: Metadata = {
-//   title: 'United Real Estate Tanger',
-//   description: 'Building affordable housing in Morocco Tanger',
-//   keywords: 'Housing, Affordable, Morocco, Tanger',
-// }
+const title = "United Real Estate Tanger"
+const description = "Building affordable housing in Morocco Tanger"
+const weburl = "https://next-blog-nu-lac.vercel.app/"
+const imageName = "https://next-blog-nu-lac.vercel.app/images/default.jpg"
+
+export const metadata: Metadata = {
+  title: title,
+  description: description,
+  keywords: 'Housing, Affordable, Morocco, Tanger',
+  openGraph: {
+    type: "website",
+    url: weburl,
+    title: title,
+    description: description,
+    siteName: title,
+    images: [{
+      url: imageName,
+    }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: title,
+    description: description,
+    creator: "KrogOlsen S.A.R.L.",
+    images: [imageName],
+  },
+}
 
 export default function RootLayout({ children }: Props) {
-  
-  const imageName ="default.jpg"
-
   return (
     <html lang="en">
-      <Head>
-        <title>United Real Estate Tanger</title>
-        <meta name="title" content="United Real Estate Tanger" />
-        <meta name="description" content="Building affordable housing in Morocco Tanger" />
+      {/* <Head>
+
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://next-blog-nu-lac.vercel.app/" />
@@ -40,7 +56,7 @@ export default function RootLayout({ children }: Props) {
         <meta property="twitter:description" content="Building affordable housing in Morocco Tanger" />
         <meta property="twitter:image" content={`https://next-blog-nu-lac.vercel.app/images/${imageName}`} />
 
-      </Head>
+      </Head> */}
 
       <body>
         <StyledComponentsRegistry>
